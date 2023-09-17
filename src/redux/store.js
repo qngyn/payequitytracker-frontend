92e1410f70685a51c2fa4ba1590a936399481b70
rica213 <25f5k252l@mozmail.com>
2023-09-17 12:38:35 +0300

    Implement user profile slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthenticationReducer from "./features/AuthenticationSlice";
 import SalaryReducer from "./features/SalarySlice";
+import UserProfileReducer from "./features/UserProfileSlice";
 
 const store = configureStore({
   reducer: {
     auth: AuthenticationReducer,
     salary: SalaryReducer,
+    userProfile: UserProfileReducer,
   },
 });
 
