6c00e983ef05665041382d28d41420a59f718754
Quynh Nguyen <qtn2@case.edu>
2023-09-17 04:34:15 -0400

    edit nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,30 @@
 import { NavLink } from "react-router-dom";
+
 const Header = () => {
-    return (
-        <nav className="header"> 
-            <div className="header-container">
-                <NavLink to="/forum" className="header-link">
-                    Forum
-                </NavLink>
+  return (
+    <nav className="header">
+      <div className="header-container">
+        <div className="left-links">
+          <NavLink to="/home" className="link-home">
+            pay equity tracker
+          </NavLink>
+        </div>
+        <div className="right-links">
+          <NavLink to="/forum" className="header-link">
+            Forum
+          </NavLink>
 
-                <NavLink to="/profile" className="header-link">
-                    Profile
-                </NavLink>
+          <NavLink to="/profile" className="header-link">
+            Profile
+          </NavLink>
 
-                <NavLink to="/" className="header-link">
-                    Log out
-                </NavLink>
-                </div>
-        </nav>
-    )
-}
+          <NavLink to="/" className="header-link">
+            Log out
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
